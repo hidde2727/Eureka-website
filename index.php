c83b22981420ca48214e1463083521056735ee73
hidde2727 <hidde.meiburg@gmail.com>
2024-06-23 17:30:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<link rel="stylesheet" href="index.css">
+
+<title>Eureka</title>
+</head>
+
+<body>
+
+<?php
+
+include("main.php");
+
+?>
+
+</body>
+</html> 
---
+++ main.php
@@ -0,0 +1,18 @@
+<div id="ideas" style="display: flex; height: 100vh; align-items: center; background-image: linear-gradient(to bottom right, #3F4F75, #7283aa);">
+
+<div class="circle" style="
+  background: #7d7a75;
+  border-bottom-right-radius: 40vh;
+  border-top-right-radius: 40vh;
+  height: 80vh;
+  width: 40vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ">
+
+<h1 style="font-size: 80px;">Ideëen</h1>
+
+</div>
+
+</div>
