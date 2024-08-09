8f0f49d57bb78ccaa2a91927fee69731c657f407
hidde2727 <93196280+hidde2727@users.noreply.github.com>
2024-08-09 10:52:15 +0200

    Added project page

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ addEventListener("load", (event) => {
         sidebarTabs[i].onmouseover = function(){
             var elements = this.getElementsByTagName("p")[0];
             const textSize = this.getElementsByTagName("p")[0].clientWidth;
-            this.style.width = "calc(30px*1.25 + " + textSize + "px)";
+            this.style.width = "calc(var(--sidebar-height) + " + textSize + "px)";
         };
         sidebarTabs[i].onmouseleave = function(){
-            this.style.width = "calc(30px*1.25)";
+            this.style.width = "var(--sidebar-height)";
         };
         sidebarTabs[i].onmousedown = function(){
             var mainWindow = document.getElementById("main-content-scroll-animation");
@@ -29,3 +29,39 @@ addEventListener("load", (event) => {
         }
     }
 });
+
+// Projects ----------------------------------
+async function GetProjects() {
+    try {
+      const response = await fetch("/Data/Projects.json");
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      const projectsPage = document.getElementById("projects");
+      for(let i = 0; i < json.length; i++) {
+        var project = document.createElement("div");
+        project.classList.add("project");
+
+        var innerHTML = '';
+        innerHTML += '<div class="project-name">' + json[i].name + '</div>';
+        innerHTML += '<div class="project-requester"><p>Aangevraagd door:</p><p>' + json[i].requester + '</p></div>';
+        innerHTML += '<div class="project-executor"><p>Uitgevoerd door:</p><p>' + json[i].executor + '</p></div>';
+        innerHTML += '<div class="project-description">' + json[i].description + '</div>';
+
+        innerHTML += '<div class="project-links">';
+        for(var j = 0; j < json[i].urls.length; j++) {
+          var url = new URL(json[i].urls[j]);
+          innerHTML += '<a href="' + json[i].urls[j] + '">' + url.hostname + '</a>';
+        }
+        innerHTML += '</div>';
+
+        project.innerHTML = innerHTML;
+        projectsPage.appendChild(project);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+GetProjects();
